57cc32c45395ff896eced58420f5bec6c496a659
Сапелкин Ярослав <yzabej@yandex.ru>
2022-12-10 11:05:04 +0200

    ChangeFragment

---
+++ app/src/main/java/com/cf/changefragments/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.cf.changefragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.cf.changefragments.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment(){
+
+    lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       binding = FragmentSecondBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button2.setOnClickListener{
+            Navigation.findNavController(binding.root).navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
+
+}
